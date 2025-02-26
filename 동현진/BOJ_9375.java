048e1c7ea94faa9365838811a54b7323c193a0f9
bronzing3412 <164727578+bronzing3412@users.noreply.github.com>
2025-02-27 01:45:27 +0900

    [Feb_4] 백준_패션왕 신해빈

---
+++ 동현진/BOJ_9375.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static HashMap<String, Integer> clothmap;
+	static ArrayList<String> cloth;
+	static int cnt;
+	
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int TC = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= TC; tc++) {
+        	cnt = 1;
+        	int n = Integer.parseInt(br.readLine());
+        	cloth = new ArrayList<String>();
+        	clothmap = new HashMap<String, Integer>();
+        	for(int i = 0; i < n; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		String s1 = st.nextToken();
+        		String s2 = st.nextToken();
+        		cloth.add(s1);
+        		clothmap.put(s2, clothmap.getOrDefault(s2, 0)+1);
+        	}
+        	
+        	for (int count : clothmap.values()) {
+                cnt *= (count + 1);
+            }
+        	cnt--;
+        	bw.write((cnt)+"\n");
+        }
+        bw.flush();
+    }
+}
