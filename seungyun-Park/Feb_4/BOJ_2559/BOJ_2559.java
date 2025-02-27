98f0c81c000d10efee1348f935edb21a0b516f7b
Park Seung Gyun <tmdrbs0925@gmail.com>
2025-02-27 17:49:29 +0900

    [Feb_4] 백준_수열

---
+++ seungyun-Park/Feb_4/BOJ_2559/BOJ_2559.java
@@ -0,0 +1,45 @@
+package _0227;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj2559 {
+	static int[] arr;
+	static int[] stackSum;
+	static int N, K, max = Integer.MIN_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		stackSum = new int[N];
+		st = new StringTokenizer(br.readLine());
+		int l = 0;
+		while(st.hasMoreTokens()) {
+			arr[l] = Integer.parseInt(st.nextToken());
+			l++;
+		}
+		max = stackSum[K-1];
+		stackSum[0] = arr[0];
+		for (int i = 1; i < N; i++) {
+			stackSum[i] = stackSum[i - 1] + arr[i];
+		}
+		
+		for(int i = K-1; i < N; i++ ) {
+		    if (i == K-1) {
+		        max = stackSum[i]; // 첫 번째 K개의 합
+		    } else {
+		        max = Math.max(max, stackSum[i] - stackSum[i-K]);
+		    }
+		}
+		System.out.println(max);
+	}//main
+	
+	
+}//bj2559
