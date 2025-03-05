d363f75c0cac64f92f6f94080f8734cc3213336e
openSongce <rkddkwl059@naver.com>
2025-03-05 14:31:36 +0900

    [Feb_4] 백준_단지번호붙이기

---
+++ 2025_Feb/Feb_4/BOJ_2667/openSongce.java
@@ -0,0 +1,81 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+	static int[][] arr;
+	static boolean[][] visited;
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {1,-1,0,0};
+	static int max;
+	
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		arr = new int[n][n];
+		visited = new boolean[n][n];
+		
+		
+		for(int i = 0; i < n; i++) {
+			String[] strs = br.readLine().split("");
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(strs[j]);
+			}
+		}
+		
+		List<Integer> list = new ArrayList();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				max = 0;
+				if(arr[i][j] > 0 && !visited[i][j]) {
+					dfs(j,i,n);
+					list.add(max);
+				}
+				
+//				for(int m = 0; m < n; m++) {
+//					for(int l = 0; l < n; l++) {
+//						System.out.print(arr[m][l] + " ");
+//					}
+//					System.out.println();
+//				}
+//				System.out.println();
+			}
+		}
+		
+		System.out.println(list.size());
+		list.sort(null);
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		
+		
+	} // main
+	
+	static void dfs(int x, int y, int n) {
+		if(arr[y][x] > 0) {
+			arr[y][x] = 0;
+			visited[y][x] = true;
+			max++;
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(nx < n && ny < n && nx >=0 && ny >= 0 && arr[ny][nx]>0) {
+					dfs(nx, ny, n);
+				}
+			}
+		}
+		
+		
+	}
+	
+	
+} // Test
