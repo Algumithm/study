3a8feb43e6b132e74a86ef748ce78974591c2c3a
Park Seung Gyun <tmdrbs0925@gmail.com>
2025-04-07 08:43:54 +0900

    [Apr_1] 백준_신입 사원

--- 2025_Apr/Apr_1/parkdu7_2.java
+++ 2025_Apr/Apr_1/parkdu7_2.java
@@ -1,14 +1,11 @@
-package _0406;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class bj1946 {
+public class Main {
 	
 	static class Person implements Comparable<Person> {
 		int a, b;
@@ -29,6 +26,7 @@ public class bj1946 {
 		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
+		List<Integer> clist = new ArrayList<Integer>();
 		for (int cs = 0; cs < T; cs++) {
 			int N = Integer.parseInt(br.readLine());
 			List<Person> list = new ArrayList<>();
@@ -43,10 +41,20 @@ public class bj1946 {
 			list.sort(null);
 			
 			
-			for (int i = 0; i < list.size(); i++) {
-				System.out.println(list.get(i).a + " " + list.get(i).b);
+			int minRank = list.get(0).b;
+			int count = 1; // 1등 뽑고 시작
+			for (int i = 1; i < list.size(); i++) {
+				if(list.get(i).b < minRank) {
+					count++;
+					minRank = list.get(i).b; //b순위가 최소 순위보다 높으면 선택
+				}
 			}
 			
+			clist.add(count);
 		}//case
+		
+		for (int i = 0; i < clist.size(); i++) {
+			System.out.println(clist.get(i));
+		}
 	}//main
 }
