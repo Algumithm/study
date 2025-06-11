ba233aa2a35b60ea3b6f603265b3126919351503
openSongce <rkddkwl059@naver.com>
2025-06-11 16:31:33 +0900

    [백준] 1719_택배

---
+++ 2025_June/June_1/openSongce_5.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] first = br.readLine().split(" ");
+        int n = Integer.parseInt(first[0]);
+        int m = Integer.parseInt(first[1]);
+
+        int[][] dist = new int[n + 1][n + 1];
+        int[][] path = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] edge = br.readLine().split(" ");
+            int u = Integer.parseInt(edge[0]);
+            int v = Integer.parseInt(edge[1]);
+            int cost = Integer.parseInt(edge[2]);
+
+            dist[u][v] = cost;
+            dist[v][u] = cost;
+
+            path[u][v] = v;
+            path[v][u] = u;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        path[i][j] = path[i][k];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print(path[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
